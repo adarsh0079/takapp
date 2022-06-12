7770eb11ab278e89f02776e2e8ce3fda431008f0
adarshweb <rajadarshofficial@gmail.com>
2022-06-12 18:16:06 +0530

    cleared unused imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import useFetchTech from "./hooks/useFetchTech";
-import useFetchAuthor from "./hooks/useFetchAuthor";
 import Home from "./page/Home";
-import NewsCard from "./components/NewsCard";
-import { ReactComponent as Loading } from "./assets/Loading.svg";
-// import { ReactComponent as Profile } from "./assets/Profile.svg";
 import Message from "./components/Message";
 import Profile from "./page/Profile";
 
